2d8c1be34626949748407024bec4738e0d710f27
Imran Ansari <i4imran322@gmail.com>
2023-02-08 00:26:22 +0530

    final commit

--- src/index.js
+++ src/index.js
@@ -7,12 +7,13 @@ import { FilterProvider } from "./context/filter_context";
 import { CartProvider } from "./context/cart_context";
 import { UserProvider } from "./context/user_context";
 import { Auth0Provider } from "@auth0/auth0-react";
+import { CLEAR_CART } from "./actions";
 // dev-pu8wyk-g.us.auth0.com
 // 7vHgXJ01aWGyMVLWdZUwQSUapigdjfKu
 ReactDOM.render(
   <Auth0Provider
-    domain={process.env.REACT_APP_AUTH_DOMAIN}
-    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
+    domain="dev-kjvy8zcejrl7xmqa.us.auth0.com"
+    clientId="JJq23DqAW8TrMDdY2DdANE6p7Ea6aPJb"
     redirectUri={window.location.origin}
     cacheLocation="localstorage"
   >
